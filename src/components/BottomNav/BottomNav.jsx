5eaf1dc618132b51f30df08c179480056e4ec402
RedisculuS <47760888+RedisculuS@users.noreply.github.com>
2024-11-25 21:15:20 +0200

    home page css done

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,5 +1,42 @@
+import { HiOutlineHome } from 'react-icons/hi2';
+import { TbListSearch } from 'react-icons/tb';
+import { HiMiniArrowsRightLeft } from 'react-icons/hi2';
+import { GoStack } from 'react-icons/go';
+import { MdOutlineAccountCircle } from 'react-icons/md';
+import s from './BottomNav.module.css';
+
 const BottomNav = () => {
-  return <div>Bottom Navigation</div>;
+  return (
+    <div className={s.wrapper}>
+      <ul className={s.navList}>
+        <li>
+          <button className={s.navItemBtn}>
+            <HiOutlineHome className={s.btnIcon} />
+          </button>
+        </li>
+        <li>
+          <button className={s.navItemBtn}>
+            <TbListSearch className={s.btnIcon} />
+          </button>
+        </li>
+        <li>
+          <button className={s.navItemBtn}>
+            <HiMiniArrowsRightLeft className={s.btnIcon} />
+          </button>
+        </li>
+        <li>
+          <button className={s.navItemBtn}>
+            <GoStack className={s.btnIcon} />
+          </button>
+        </li>
+        <li>
+          <button className={s.navItemBtn}>
+            <MdOutlineAccountCircle className={s.btnIcon} />
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default BottomNav;
